8132f621c7ac45c3ffd68382ef86071c0b1c6acb
coolxerxes <124738348+coolxerxes@users.noreply.github.com>
2023-07-11 00:26:07 -0700

    updated

---
+++ CisecoNextjs/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import "@/fonts/line-awesome-1.3.0/css/line-awesome.css";
+import "@/styles/index.scss";
+import "rc-slider/assets/index.css";
+import Footer from "@/shared/Footer/Footer";
+import SiteHeader from "@/app/SiteHeader";
+import CommonClient from "./CommonClient";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "optional",
+  weight: ["100", "300", "400", "500", "600", "700", "800"],
+});
+
+export default function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: any;
+}) {
+  return (
+    <html lang="en" className={poppins.className}>
+      <body className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
+        <SiteHeader />
+        {children}
+        <CommonClient />
+        <Footer />
+      </body>
+    </html>
+  );
+}
